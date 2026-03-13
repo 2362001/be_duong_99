486523b395e067b6e433339a639ab67ab702121b
ngtienthanh <ngtienthanh@vnpt.vn>
2026-03-13 21:08:07 +0700

    .

---
+++ src/main/java/vn/be/platform_service/entity/Product.java
@@ -0,0 +1,19 @@
+package vn.be.platform_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+    @SequenceGenerator(name = "product_seq", sequenceName = "product_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    private String description;
+    private String price;
+}
